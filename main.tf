60ad3842f366ddb168c700b993492a3185478ad7
Ben Watrous <ben.watrous@microsoft.com>
2019-11-26 16:34:15 -0800

    successful plan

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ provider "azurerm" {
 
 
 resource "azurerm_virtual_machine" "cc_tf_vm" {
-  name                  = "${local.virtual_machine_name}"
-  resource_group_name   = "${azurerm_resource_group.cc_tf_rg.name}"
-  location              = "${azurerm_resource_group.cc_tf_rg.location}"
-  network_interface_ids = ["${azurerm_network_interface.cc_tf_nic.id}"]
-  vm_size               = "${var.machine_type}"
+  name                  = local.virtual_machine_name
+  resource_group_name   = azurerm_resource_group.cc_tf_rg.name
+  location              = azurerm_resource_group.cc_tf_rg.location
+  network_interface_ids = [azurerm_network_interface.cc_tf_nic.id]
+  vm_size               = var.machine_type
 
   delete_os_disk_on_termination = true
   delete_data_disks_on_termination = true
@@ -48,6 +48,8 @@ resource "azurerm_virtual_machine" "cc_tf_vm" {
   }
 }
 
+data "azurerm_subscription" "current" {}
+
 data "azurerm_builtin_role_definition" "contributor" {
   name = "Contributor"
 }
@@ -55,8 +57,8 @@ data "azurerm_builtin_role_definition" "contributor" {
 
 resource "azurerm_role_assignment" "cc_tf_mi_role" {
   name               = "${azurerm_virtual_machine.cc_tf_vm.name}"
-  scope              = "${data.azurerm_subscription.primary.id}"
-  role_definition_id = "${data.azurerm_subscription.subscription.id}${data.azurerm_builtin_role_definition.contributor.id}"
+  scope              = "${data.azurerm_subscription.current.id}"
+  role_definition_id = "${data.azurerm_subscription.current.id}${data.azurerm_builtin_role_definition.contributor.id}"
   principal_id       = "${lookup(azurerm_virtual_machine.cc_tf_vm.identity[0], "principal_id")}"
 }
 
@@ -69,12 +71,11 @@ resource "azurerm_virtual_machine_extension" "fetch_cyclecloud_install_script" {
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.4"
-  depends_on           = ["${azurerm_virtual_machine.cc_tf_vm}"]
+  depends_on           = [azurerm_virtual_machine.cc_tf_vm]
 
   settings = <<SETTINGS
     {
-        "commandToExecute": "curl -k -L -o /tmp/install_cyclecloud.py \
-            'https://raw.githubusercontent.com/bwatrous/cyclecloud-terraform/master/scripts/cyclecloud_install.py'"
+        "commandToExecute": "curl -k -L -o /tmp/install_cyclecloud.py https://raw.githubusercontent.com/bwatrous/cyclecloud-terraform/master/scripts/cyclecloud_install.py"
     }
 SETTINGS
 }
@@ -88,7 +89,7 @@ resource "azurerm_virtual_machine_extension" "install_cyclecloud" {
   publisher            = "Microsoft.Compute"
   type                 = "CustomScriptExtension"
   type_handler_version = "1.4"
-  depends_on = ["${azurerm_virtual_machine_extension.fetch_cyclecloud_install_script}"]
+  depends_on = [azurerm_virtual_machine_extension.fetch_cyclecloud_install_script]
 
 #   settings = <<SETTINGS
 #     {
@@ -101,9 +102,7 @@ resource "azurerm_virtual_machine_extension" "install_cyclecloud" {
     
   settings = <<SETTINGS
     {
-        "commandToExecute": "python /tmp/install_cyclecloud.py --acceptTerms --useManagedIdentity \
-            --username=${var.cyclecloud_username} --password=${var.cyclecloud_password} \
-            --storageAccount=${var.cyclecloud_storage_account} --hostname=${var.cyclecloud_dns_label}"
+        "commandToExecute": "python /tmp/install_cyclecloud.py --acceptTerms --useManagedIdentity --username=${var.cyclecloud_username} --password=${var.cyclecloud_password} --storageAccount=${var.cyclecloud_storage_account} --hostname=${var.cyclecloud_dns_label}"
     }
 SETTINGS
 }
